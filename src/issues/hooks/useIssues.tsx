485c34abfe3732313774c29d39e88d8ba8d45069
Antonio Espinoza <anespinoza@baufest.com>
2024-04-19 17:39:47 -0400

    add pagination and infinite scroll issues

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+
 import { githubApi } from '../../api/githubApi';
 import { Issue, State } from '../interfaces';
 import { sleep } from '../../helpers/sleep';
@@ -7,22 +9,23 @@ import { sleep } from '../../helpers/sleep';
 interface Props {
     state?: State;
     labels: string[];
+    page?:   number;
 }
 
-const getIssues = async (labels: string[] = [], state?: State,): Promise<Issue[]> => {
 
-    await sleep(2);
-
+const getIssues = async({ labels, state, page = 1 }: Props):Promise<Issue[]> => {
+    
+    // await sleep(2);
     const params = new URLSearchParams();
 
-    if (state) params.append('state', state);
+    if ( state ) params.append('state', state );
 
-    if (labels.length > 0) {
-        const labelString = labels.join(','); //separamos los labels por , 
-        params.append('labels', labelString);
+    if ( labels.length > 0 ) {
+        const labelString = labels.join(',');
+        params.append('labels',labelString);
     }
 
-    params.append('page', '1');
+    params.append('page', page.toString() );
     params.append('per_page', '5');
 
     const { data } = await githubApi.get<Issue[]>('/issues', { params });
@@ -31,13 +34,39 @@ const getIssues = async (labels: string[] = [], state?: State,): Promise<Issue[]
 }
 
 
-export const useIssues = ({ state, labels }: Props) => {
+export const useIssues = ({ state, labels }: Props ) => {
+
+    const [page, setPage] = useState(1);
+
+    useEffect(() => { //devolvernos a la página 1 cuando cambie el state o los labels
+      setPage(1);
+    }, [state, labels])
+    
+  
     const issuesQuery = useQuery({
-        queryKey: ['issues', {state, labels}],
-        queryFn: () => getIssues(labels, state),
+        queryKey: ['issues', {state, labels, page}],
+        queryFn: () => getIssues({labels, state, page}),
     });
 
+    const nextPage = () => {
+        if ( issuesQuery.data?.length === 0 ) return;
+        setPage( page + 1 );        
+        // issuesQuery.refetch();
+    }
+
+    const prevPage = () => {
+        if ( page > 1 ) setPage( page -1 );
+    }
+
     return {
+        // Properties
         issuesQuery,
+
+        // Getter
+        page: issuesQuery.isFetching ? 'Loading': page,
+
+        // Methods
+        nextPage,
+        prevPage,
     }
 }
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -13,7 +13,7 @@ export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
   const [state, setState] = useState<State>();
 
-  const { issuesQuery } = useIssues({ state, labels: selectedLabels });
+  const { issuesQuery, page, nextPage, prevPage } = useIssues({ state, labels: selectedLabels });
 
   const onLabelChanged = (labelName: string) => {
     (selectedLabels.includes(labelName))
@@ -37,6 +37,25 @@ export const ListView = () => {
               />
             )
         }
+
+        <div className='d-flex mt-2 justify-content-between align-items-center'>
+          <button
+            className='btn btn-outline-primary'
+            disabled={issuesQuery.isFetching}
+            onClick={prevPage}>
+            Prev
+          </button>
+
+          <span>{page}</span>
+
+          <button
+            className='btn btn-outline-primary'
+            disabled={issuesQuery.isFetching}
+            onClick={nextPage}>
+            Next
+          </button>
+        </div>
+
       </div>
 
       <div className="col-4">
